66f0c959421eb92b59cf1ed2fee317c4c21c00d7
Peter Pagac <QuarKUS7@gmail.com>
2019-04-27 22:57:49 +0200

    app running locally

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
-def home():
-    return 'Hello World'
+class Rates(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+api.add_resource(Rates, '/')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', debug=True)
