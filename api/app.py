4d9c18fb8960e2388a49f1e4b08d6c2f2163b2f7
Peter Pagac <QuarKUS7@gmail.com>
2019-04-28 13:58:47 +0200

    redis added to stack

--- api/app.py
+++ api/app.py
@@ -1,12 +1,20 @@
 from flask import Flask
 from flask_restful import Resource, Api
+import redis
 
 app = Flask(__name__)
 api = Api(app)
 
+def get_hit_count():
+        r.incr('hits')
+
+r = redis.Redis(
+    host='redis'
+)
 class Rates(Resource):
     def get(self):
-        return {'hello': 'world'}
+        get_hit_count()
+        return {'hello': int(r.get('hits'))}
 
 api.add_resource(Rates, '/')
 
