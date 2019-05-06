32bb2262c02b506f2b565c39f93a4d727c421e3d
QuarKUS7 <QuarKUS7@gmail.com>
2019-05-06 23:33:51 +0200

    comments added

--- api/convert.py
+++ api/convert.py
@@ -17,16 +17,19 @@ class Convert():
         self.to_curren = self._check_currency_symbol(to_curren)
 
     def _load_symbols(self):
+        # Load symbol list
         with open('symbols.json', 'r', encoding='utf-8') as f:
             symbol_list = json.load(f)
         return list(symbol_list.values())
 
     def _check_currency_symbol(self, curren):
+        """Check if symbol in symbol list. Returns code of the currency"""
         if curren == 'All':
             return 'All'
         return next((item['code'] for item in self._symbol_list if curren.upper() in [item['symbol'], item['code']]), None)
             
     def convert(self):
+        """Function for tringering conversion on initialized Convert object"""
         if not self.from_curren:
             return {"Error":{"input_currency":["Unknown input currency or symbol."]}}, 400
         self.from_curren = self._get_rate(self.from_curren)
@@ -39,18 +42,18 @@ class Convert():
         return out
 
     def _change(self, currency):
+        # Formula for currency chnage
         return float(next(iter(self.from_curren.values()))) * float(self.amount) / float(currency)
         
     def _request(self):
-        try:
-            response = requests.get(self._CNB_URL)
-        except:
-            return {'Error': 'something went wronge try again later'}
-
+        """Request txt from CNB"""
+        response = requests.get(self._CNB_URL)
         if response.content:
             return response.text
 
     def _get_rate(self, currency):
+        """Return rates from db or trigers update for rates"""
+        # All is for missing output currency
         if currency == 'All':
             rates = self.r.hgetall('rates')
             if rates:
@@ -66,15 +69,18 @@ class Convert():
             return {currency: self.r.hget('rates', currency)}
 
     def _update_rates(self):
+        """Parse and insert new rates to db"""
         resp = self._request()
         rates_dict = self._parse_cnb(resp)
         self._insert_into_redis(rates_dict)
 
     def _parse_cnb(self, text):
+        """Returns dict of currency and rate from CNB rates"""
         rates_dict = {}
         breakpoint()
         for row in text.split('\n')[2:-1]:
             rates_dict[row.split('|')[-2]] = float(row.split('|')[-1].replace(',','.')) / float(row.split('|')[-3].replace(',','.'))
+        # CZK not in the list so added manually
         rates_dict['CZK'] = '1'
         return rates_dict
     
