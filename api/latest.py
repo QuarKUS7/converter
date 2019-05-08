01e28ffce87f94f3a919bea55b359ed21c19b527
QuarKUS7 <QuarKUS7@gmail.com>
2019-05-08 19:08:30 +0200

    custom list and rebase working, needs to be refacored

--- api/latest.py
+++ api/latest.py
@@ -12,3 +12,12 @@ class Latest(Base):
         elif self.custom_list and not self.base:
             rates = self._get_or_update()
             return {key:value for (key, value) in rates.items() if key in self.custom_list}
+        elif not self.custom_list and self.base:
+            return self._rebase(self._get_or_update())
+        elif self.custom_list and self.base:
+            rates = self._get_or_update()
+            return self._rebase({key:value for (key, value) in rates.items() if key in self.custom_list}, self.base)
+
+    def _rebase(self, dict_rates):
+        base_rate = dict_rates[self.base]
+        return dict((k, float(v)/float(base_rate)) for k, v in dict_rates.items())
