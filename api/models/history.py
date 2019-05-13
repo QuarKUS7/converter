5e6707601c19470d8d4c3814b3c90ab8599e66ed
QuarKUS7 <QuarKUS7@gmail.com>
2019-05-13 17:04:07 +0200

    refactoring done

--- api/models/history.py
+++ api/models/history.py
@@ -1,21 +1,14 @@
 from .latest import Latest
+from utils import format_to_dot_date
+
 
 class History(Latest):
-
-    def __init__(self, date):
+    def __init__(self, base, rates, date):
+        super().__init__(base, rates)
         self.date = str(date)
-        self.base = 'CZK'
 
     def fetch_hist_rates(self):
-        self._CNB_URL = self._CNB_URL + f'?date={self.format_date(self.date)}'
-        return self._update_rates()
-
-    def format_date(self, date):
-        yyyy, mm, dd = str(date).split("-")
-        return f"{dd}.{mm}.{yyyy}"
-
-    def _update_rates(self):
-        """Parse and insert new rates to db"""
+        self._CNB_URL = self._CNB_URL + f'?date={format_to_dot_date(self.date)}'
         resp = self._request()
         rates_dict = self._parse_cnb(resp)
-        return {"base": self.base, "rates": self._rebase(rates_dict, 1), "date": self.date}
+        return {"base": self.base, "rates": self._rebase(rates_dict, self.base_rate), "date": self.date}
